6091a834e861b8b516b637fec119bc575a8e143d
Quintus Marais <quintus.marais@kwilibri.com>
2022-07-02 16:37:16 +0200

    Add console app to exercise the library

---
+++ src/DriverConsole/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open Loans
+
+let loanTransactions =
+    [
+        {
+            TransactionDate = DateOnly(2021, 10, 21)
+            Amount = 2_000_000M
+            TransactionType = MoneyLent
+        }
+        {
+            TransactionDate = DateOnly(2021, 10, 26)
+            Amount = 10_000M
+            TransactionType = MoneyRepaid
+        }
+        {
+            TransactionDate = DateOnly(2021, 11, 30)
+            Amount = 30_000M
+            TransactionType = MoneyRepaid
+        }
+        {
+            TransactionDate = DateOnly(2021, 12, 22)
+            Amount = 30_000M
+            TransactionType = MoneyRepaid
+        }
+
+    ]
+
+for transaction in loanTransactions do
+    printfn "%s" (transaction.TransactionDate.ToShortDateString())
