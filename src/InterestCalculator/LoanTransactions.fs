aede098a25c9f8bbe324d0aad9122d89cbf06edd
Quintus Marais <quintus.marais@kwilibri.com>
2022-07-02 09:57:11 +0200

    Begin defining the types to model a loan

---
+++ src/InterestCalculator/LoanTransactions.fs
@@ -0,0 +1,36 @@
+﻿module Loans
+
+open System
+
+type LoanTransactionType =
+    | MoneyLent
+    | MoneyRepaid
+
+type LoanTransaction =
+    { TransactionDate: DateTime
+      Amount: Decimal
+      TransactionType: LoanTransactionType }
+
+type RateInterval =
+    | Daily
+    | Weekly
+    | Monthly
+    | Annually
+
+type InterestRateDefinition =
+    { RateInterval: RateInterval
+      Rate: Decimal }
+
+type RateChange =
+    { Date: Date
+      RateDefinition: InterestRateDefinition }
+
+
+
+let loanTransactions =
+    [ { TransactionDate = DateTime(2021, 1, 1)
+        Amount = 200M
+        TransactionType = MoneyLent }
+      { TransactionDate = DateTime(2022, 1, 1)
+        Amount = 100M
+        TransactionType = MoneyRepaid } ]
