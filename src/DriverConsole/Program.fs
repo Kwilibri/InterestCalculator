d3baeb545ea663cf42b314adad3765cdbff6244f
Quintus Marais <quintus.marais@kwilibri.com>
2022-07-26 11:26:52 +0200

    Update the dataset

--- src/DriverConsole/Program.fs
+++ src/DriverConsole/Program.fs
@@ -4,40 +4,78 @@ open Loans
 let loanTransactions =
     [
         {
-            TransactionDate = DateOnly(2021, 10, 21)
+            TransactionDate = DateOnly(2021, 03, 31)
             Amount = 2_000_000M
             TransactionType = MoneyLent
         }
         {
-            TransactionDate = DateOnly(2021, 10, 26)
-            Amount = 10_000M
-            TransactionType = MoneyRepaid
+            TransactionDate = DateOnly(2021, 04, 12)
+            Amount = 10_000_000M
+            TransactionType = MoneyLent
         }
         {
-            TransactionDate = DateOnly(2021, 11, 30)
-            Amount = 30_000M
-            TransactionType = MoneyRepaid
+            TransactionDate = DateOnly(2021, 04, 23)
+            Amount = 3_000_000M
+            TransactionType = MoneyLent
         }
         {
-            TransactionDate = DateOnly(2021, 12, 22)
-            Amount = 30_000M
-            TransactionType = MoneyRepaid
+            TransactionDate = DateOnly(2021, 12, 02)
+            Amount = 2_000_000M
+            TransactionType = MoneyLent
         }
 
     ]
 
 let rateDefinition =
-    {
-        RateInterval = Annually
-        Rate = 0.18M
+    [
+        {
+            RateStartDate = DateOnly(2021,03,31)
+            RateInterval = Annually
+            Rate = 0.1200M
+        }
+        {
+            RateStartDate = DateOnly(2022,01,28)
+            RateInterval = Annually
+            Rate = 0.1250M
+        }
+        {
+            RateStartDate = DateOnly(2022,03,25)
+            RateInterval = Annually
+            Rate = 0.1275M
+        }
+        {
+            RateStartDate = DateOnly(2022,05,20)
+            RateInterval = Annually
+            Rate = 0.1325M
+        }
+        {
+            RateStartDate = DateOnly(2022,07,21)
+            RateInterval = Annually
+            Rate = 0.1400M
+        }
 
-    }
+
+
+
+    ]
 
 let loan =
-    { RateDefinition =  rateDefinition; LoanTransactions = loanTransactions }
+    { RateChanges =  rateDefinition; LoanTransactions = loanTransactions }
 
-let dailyEquivalentRateDefinition =
-    convertRateDefinitionInterval( rateDefinition, Monthly)
+let annualEquivalentRate =
+    convertRateDefinitionInterval( rateDefinition.Head, Annually)
+
+
+let loanScheduleTargetDate = DateOnly(2022,06,30)
+let calculateLoanAtoms(loan:Loan) =
+    /// Need to calculate loan atom for each transaction and each interest change.
+
+
+let mutable interest = 0.0M
+for transaction in loan.LoanTransactions do
+
+    interest <-  interest + dailyCompoundedInterestForInterval(transaction.Amount ,annualEquivalentRate.Rate,DateOnly(2021, 10, 21),DateOnly(2021, 10, 26))
+
 
 for transaction in loanTransactions do
     printfn "%s" (transaction.TransactionDate.ToShortDateString())
