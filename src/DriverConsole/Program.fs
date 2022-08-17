f844b009c7b70a2a8c2b6300ee956aa7c1a70373
Quintus Marais <quintus.marais@kwilibri.com>
2022-08-17 20:21:56 +0200

    Formatting updated via fantomas

--- src/DriverConsole/Program.fs
+++ src/DriverConsole/Program.fs
@@ -4,70 +4,48 @@ open System.Diagnostics.Metrics
 open System.Diagnostics
 
 let loanTransactions =
-    [|
-        {
-            TransactionDate = DateOnly(2021, 03, 31)
-            Amount = 2_000_000M
-            TransactionType = MoneyLent
-        }
-        {
-            TransactionDate = DateOnly(2021, 04, 23)
-            Amount = 3_000_000M
-            TransactionType = MoneyLent
-        }
-        {
-            TransactionDate = DateOnly(2021, 04, 12)
-            Amount = 10_000_000M
-            TransactionType = MoneyLent
-        }
-        {
-            TransactionDate = DateOnly(2021, 12, 02)
-            Amount = 2_000_000M
-            TransactionType = MoneyLent
-        }
+    [| { TransactionDate = DateOnly(2021, 3, 31)
+         Amount = 2_000_000M
+         TransactionType = MoneyLent }
+       { TransactionDate = DateOnly(2021, 4, 23)
+         Amount = 3_000_000M
+         TransactionType = MoneyLent }
+       { TransactionDate = DateOnly(2021, 4, 12)
+         Amount = 10_000_000M
+         TransactionType = MoneyLent }
+       { TransactionDate = DateOnly(2021, 12, 2)
+         Amount = 2_000_000M
+         TransactionType = MoneyLent }
 
-    |]
+       |]
 
 let rateChanges =
-    [|
-        {
-            RateStartDate = DateOnly(2022,01,28)
-            RateInterval = Annually
-            Rate = 0.1250M
-        }
-        {
-            RateStartDate = DateOnly(2021,03,31)
-            RateInterval = Annually
-            Rate = 0.1200M
-        }
-        {
-            RateStartDate = DateOnly(2021,11,19)
-            RateInterval = Annually
-            Rate = 0.1225M
-        }
-        {
-            RateStartDate = DateOnly(2022,03,25)
-            RateInterval = Annually
-            Rate = 0.1275M
-        }
-        {
-            RateStartDate = DateOnly(2022,05,20)
-            RateInterval = Annually
-            Rate = 0.1325M
-        }
-        {
-            RateStartDate = DateOnly(2022,07,21)
-            RateInterval = Annually
-            Rate = 0.1400M
-        }
+    [| { RateStartDate = DateOnly(2022, 1, 28)
+         RateInterval = Annually
+         Rate = 0.1250M }
+       { RateStartDate = DateOnly(2021, 3, 31)
+         RateInterval = Annually
+         Rate = 0.1200M }
+       { RateStartDate = DateOnly(2021, 11, 19)
+         RateInterval = Annually
+         Rate = 0.1225M }
+       { RateStartDate = DateOnly(2022, 3, 25)
+         RateInterval = Annually
+         Rate = 0.1275M }
+       { RateStartDate = DateOnly(2022, 5, 20)
+         RateInterval = Annually
+         Rate = 0.1325M }
+       { RateStartDate = DateOnly(2022, 7, 21)
+         RateInterval = Annually
+         Rate = 0.1400M }
 
-    |]
+       |]
 
 let loan =
-    { RateChanges =  rateChanges; LoanTransactions = loanTransactions }
+    { RateChanges = rateChanges
+      LoanTransactions = loanTransactions }
 
-let annualEquivalentRate =
-    convertRateDefinitionInterval( rateChanges[0], Annually)
+let annualEquivalentRate = convertRateDefinitionInterval (rateChanges[0], Annually)
 
 
 type loanCalculationErrors =
@@ -76,121 +54,165 @@ type loanCalculationErrors =
     | TargetDateTooLate
 
 
-let calculateInterestChangeLoanAtoms(loan:Loan,calculationEndDate:DateOnly) =
-    let sortedRateChanges =  loan.RateChanges |> Array.sortBy(fun x -> x.RateStartDate)
-    let sortedTransactions = loan.LoanTransactions |> Array.sortBy(fun x -> x.TransactionDate)
+let calculateInterestChangeLoanAtoms (loan: Loan, calculationEndDate: DateOnly) =
+    let sortedRateChanges =
+        loan.RateChanges
+        |> Array.sortBy (fun x -> x.RateStartDate)
+
+    let sortedTransactions =
+        loan.LoanTransactions
+        |> Array.sortBy (fun x -> x.TransactionDate)
+
     let firstTransactionDate = sortedTransactions.[0].TransactionDate
     let firstKnownRateDate = sortedRateChanges.[0].RateStartDate
+
     let result =
         if (firstKnownRateDate > firstTransactionDate) then
             Error RateStartDateTooLate
         else
             // Create a transaction atom for each rate change
             let atomsCount = sortedRateChanges.Length
-            let createAtomForIndexedItem(sortedRateChanges:InterestRateDefinition[],index,calculationEndDate) =
+
+            let createAtomForIndexedItem (sortedRateChanges: InterestRateDefinition [], index, calculationEndDate) =
                 let item = sortedRateChanges[index]
-                let dailyRate = convertRateDefinitionInterval(item,Daily)
+                let dailyRate = convertRateDefinitionInterval (item, Daily)
                 let startDate = item.RateStartDate
+
                 let endDate =
-                    if (index = sortedRateChanges.Length-1) then
+                    if (index = sortedRateChanges.Length - 1) then
                         calculationEndDate
                     else
-                        sortedRateChanges[index+1].RateStartDate.AddDays(-1)
+                        sortedRateChanges[index + 1]
+                            .RateStartDate.AddDays(-1)
+
                 let result =
-                    {
-                        StartDate = startDate
-                        EndDate = endDate
-                        OpeningBalance = 0.0M
-                        EffectiveDailyRate = dailyRate.Rate
-                    }
+                    { StartDate = startDate
+                      EndDate = endDate
+                      OpeningBalance = 0.0M
+                      EffectiveDailyRate = dailyRate.Rate }
+
                 result
 
-            let atoms = Array.init atomsCount (fun index -> createAtomForIndexedItem(sortedRateChanges,index,calculationEndDate))
+            let atoms =
+                Array.init atomsCount (fun index ->
+                    createAtomForIndexedItem (sortedRateChanges, index, calculationEndDate))
+
             Ok atoms
+
     result
 
-let activeRateForGivenDate(interestCalculationAtoms:InterestCalculationAtom[],targetDate:DateOnly) =
+let activeRateForGivenDate (interestCalculationAtoms: InterestCalculationAtom [], targetDate: DateOnly) =
     let count = interestCalculationAtoms.Length
+
     if (targetDate < interestCalculationAtoms[0].StartDate) then
         Error TargetDateTooEarly
-    elif (targetDate > interestCalculationAtoms[count-1].EndDate) then
+    elif (targetDate > interestCalculationAtoms[count - 1].EndDate) then
         Error TargetDateTooLate
     else
         let mutable candidate = 0.0M
         let mutable iterator = 0
         let mutable startDate = interestCalculationAtoms[iterator].StartDate
         let mutable endDate = interestCalculationAtoms[iterator].EndDate
-        let maxDate = interestCalculationAtoms[count-1].EndDate
+        let maxDate = interestCalculationAtoms[count - 1].EndDate
 
         while (endDate <= maxDate) do
             if (targetDate >= startDate && targetDate <= endDate) then
-                candidate <- interestCalculationAtoms[iterator].EffectiveDailyRate
+                candidate <-
+                    interestCalculationAtoms[iterator]
+                        .EffectiveDailyRate
+
             iterator <- iterator + 1
-            if (iterator <= count-1) then
+
+            if (iterator <= count - 1) then
                 startDate <- interestCalculationAtoms[iterator].StartDate
                 endDate <- interestCalculationAtoms[iterator].EndDate
             else
                 endDate <- maxDate.AddDays(1)
+
         Ok candidate
 
 
-let createAtomForIndexedTransaction(
-        sortedTransactions:LoanTransaction[],
-        interestChangeAtoms:InterestCalculationAtom[],
-        index:int,
-        calculationEndDate:DateOnly)=
+let createAtomForIndexedTransaction
+    (
+        sortedTransactions: LoanTransaction [],
+        interestChangeAtoms: InterestCalculationAtom [],
+        index: int,
+        calculationEndDate: DateOnly
+    ) =
     let item = sortedTransactions[index]
-    let dailyRateResult = activeRateForGivenDate(interestChangeAtoms,item.TransactionDate)
+
+    let dailyRateResult =
+        activeRateForGivenDate (interestChangeAtoms, item.TransactionDate)
+
     let dailyRate =
         match dailyRateResult with
         | Ok item -> item
         | Error err -> failwith "Couldn't calculate the applicable interest rate"
+
     let startDate = item.TransactionDate
+
     let endDate =
-        if (index = sortedTransactions.Length-1) then
-              calculationEndDate
-          else
-              sortedTransactions[index+1].TransactionDate.AddDays(-1)
+        if (index = sortedTransactions.Length - 1) then
+            calculationEndDate
+        else
+            sortedTransactions[index + 1]
+                .TransactionDate.AddDays(-1)
+
     let openingBalance = item.Amount
+
     let result =
-        {
-            StartDate = startDate
-            EndDate = endDate
-            OpeningBalance = openingBalance
-            EffectiveDailyRate = dailyRate
-        }
+        { StartDate = startDate
+          EndDate = endDate
+          OpeningBalance = openingBalance
+          EffectiveDailyRate = dailyRate }
+
     result
 
 
 
 
-let calculateTransactionLoanAtoms(loan:Loan,calculationEndDate:DateOnly) =
-    let sortedTransactions = loan.LoanTransactions |> Array.sortBy(fun x -> x.TransactionDate)
+let calculateTransactionLoanAtoms (loan: Loan, calculationEndDate: DateOnly) =
+    let sortedTransactions =
+        loan.LoanTransactions
+        |> Array.sortBy (fun x -> x.TransactionDate)
+
     let atomsCount = sortedTransactions.Length
-    let interestChangeAtomsResult = calculateInterestChangeLoanAtoms(loan, calculationEndDate)
+
+    let interestChangeAtomsResult =
+        calculateInterestChangeLoanAtoms (loan, calculationEndDate)
+
     let interestChangeAtoms =
         match interestChangeAtomsResult with
         | Ok item -> item
         | Error err -> failwith "incorrect Date"
 
-    let atoms = Array.init atomsCount (fun index -> createAtomForIndexedTransaction(sortedTransactions,interestChangeAtoms,index,calculationEndDate))
+    let atoms =
+        Array.init atomsCount (fun index ->
+            createAtomForIndexedTransaction (sortedTransactions, interestChangeAtoms, index, calculationEndDate))
+
     atoms
 
 
-let loanCalculationEndDate = DateOnly(2022,07,26)
-let interestChangeAtomsResult = calculateInterestChangeLoanAtoms(loan, loanCalculationEndDate)
-let transactionAtoms = calculateTransactionLoanAtoms(loan, loanCalculationEndDate)
+let loanCalculationEndDate = DateOnly(2022, 7, 26)
+
+let interestChangeAtomsResult =
+    calculateInterestChangeLoanAtoms (loan, loanCalculationEndDate)
+
+let transactionAtoms = calculateTransactionLoanAtoms (loan, loanCalculationEndDate)
 
 let interestChangeAtoms =
     match interestChangeAtomsResult with
     | Ok item -> item
     | Error _ -> failwith "There was an error calculation interest change transactions"
+
 printfn "Printing interest rate transaction atoms"
+
 for item in interestChangeAtoms do
     printfn "%A" item
 
 printfn "--------------------------------------------------------------------------------"
 printfn "Printing transaction atoms"
+
 for transaction in transactionAtoms do
     printfn "%A" transaction
 
--- src/InterestCalculator/LoanTransactions.fs
+++ src/InterestCalculator/LoanTransactions.fs
@@ -25,8 +25,7 @@ type InterestCalculationAtom =
     { StartDate: DateOnly
       EndDate: DateOnly
       OpeningBalance: Decimal
-      EffectiveDailyRate: Decimal
-    }
+      EffectiveDailyRate: Decimal }
 
 type RateIntervalType =
     | Daily
@@ -35,24 +34,22 @@ type RateIntervalType =
 
 
 type InterestRateDefinition =
-    {
-      RateStartDate: DateOnly
+    { RateStartDate: DateOnly
       RateInterval: RateIntervalType
       Rate: Decimal }
 
 
 type Loan =
-    {
-        RateChanges: InterestRateDefinition []
-        LoanTransactions: LoanTransaction []
-    }
+    { RateChanges: InterestRateDefinition []
+      LoanTransactions: LoanTransaction [] }
 
 
 
 
 /// Convert the InterestRateDefinition to use an equivalent rate with a different interval
-let convertRateDefinitionInterval(interestRateDefinition:InterestRateDefinition,targetInterval:RateIntervalType) =
+let convertRateDefinitionInterval (interestRateDefinition: InterestRateDefinition, targetInterval: RateIntervalType) =
     let originalInterval = interestRateDefinition.RateInterval
+
     let result =
         match originalInterval with
         | Daily ->
@@ -64,54 +61,70 @@ let convertRateDefinitionInterval(interestRateDefinition:InterestRateDefinition,
                 // First convert to annual
                 let annualRate = interestRateDefinition.Rate * 365M
                 let monthlyRate = annualRate / 12M
-                {
-                    RateStartDate = interestRateDefinition.RateStartDate
-                    RateInterval = Monthly;
-                    Rate = monthlyRate
-                }
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = Monthly
+                  Rate = monthlyRate }
             | Annually ->
                 let annualRate = interestRateDefinition.Rate * 365M
-                {
-                    RateStartDate = interestRateDefinition.RateStartDate
-                    RateInterval = Annually;
-                    Rate = annualRate
-                }
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = Annually
+                  Rate = annualRate }
         | Monthly ->
             match targetInterval with
             | Daily ->
                 let annualRate = interestRateDefinition.Rate * 12M
                 let dailyRate = annualRate / 365M
-                {
-                    RateStartDate = interestRateDefinition.RateStartDate
-                    RateInterval = targetInterval;
-                    Rate = dailyRate}
-            | Monthly ->
-                interestRateDefinition
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = targetInterval
+                  Rate = dailyRate }
+            | Monthly -> interestRateDefinition
             | Annually ->
                 let annualRate = interestRateDefinition.Rate * 12M
-                {RateStartDate = interestRateDefinition.RateStartDate;RateInterval = targetInterval; Rate = annualRate}
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = targetInterval
+                  Rate = annualRate }
         | Annually ->
             match targetInterval with
             | Daily ->
-                let dailyRate = interestRateDefinition.Rate/365M
-                {RateStartDate = interestRateDefinition.RateStartDate;RateInterval = targetInterval; Rate = dailyRate}
+                let dailyRate = interestRateDefinition.Rate / 365M
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = targetInterval
+                  Rate = dailyRate }
             | Monthly ->
-                let monthlyRate = interestRateDefinition.Rate/12M
-                {RateStartDate = interestRateDefinition.RateStartDate;RateInterval = targetInterval; Rate = monthlyRate}
-            | Annually ->
-                interestRateDefinition
+                let monthlyRate = interestRateDefinition.Rate / 12M
+
+                { RateStartDate = interestRateDefinition.RateStartDate
+                  RateInterval = targetInterval
+                  Rate = monthlyRate }
+            | Annually -> interestRateDefinition
+
     result
 
-let dailyCompoundedInterestForInterval(openingBalance:decimal,dailyRate:Decimal,startDate:DateOnly,endDate:DateOnly):Decimal =
+let dailyCompoundedInterestForInterval
+    (
+        openingBalance: decimal,
+        dailyRate: Decimal,
+        startDate: DateOnly,
+        endDate: DateOnly
+    ) : Decimal =
     // See this stackoverflow answer.
     //https://stackoverflow.com/questions/6425501/is-there-a-math-api-for-powdecimal-decimal
     // .net decided to not implement pow for decimal, leading to these ridiculous hacks
-  let precisionMagnifier = (double)1_000_000.0
-  let numberOfDays = (double)(endDate.DayNumber  - startDate.DayNumber)
-  let exponent = numberOfDays/365.*1.0  //Assumes incoming rate = annual
-  let doubleResult = (double)openingBalance * (1.0 + (double(dailyRate)))**(exponent)-(double)openingBalance
-  let increasedPrecisionResult = (doubleResult * precisionMagnifier)
-  let intResult = (decimal)increasedPrecisionResult
-  let result = (decimal)intResult / (decimal)precisionMagnifier
-  result
+    let precisionMagnifier = (double) 1_000_000.0
+    let numberOfDays = (double) (endDate.DayNumber - startDate.DayNumber)
+    let exponent = numberOfDays / 365. * 1.0 //Assumes incoming rate = annual
 
+    let doubleResult =
+        (double) openingBalance
+        * (1.0 + (double (dailyRate))) ** (exponent)
+        - (double) openingBalance
+
+    let increasedPrecisionResult = (doubleResult * precisionMagnifier)
+    let intResult = (decimal) increasedPrecisionResult
+    let result = (decimal) intResult / (decimal) precisionMagnifier
+    result
